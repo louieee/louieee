f3dade7882275f3ff3fae335a318401599219834
Louis Ohaegbu <uzochukwu.ohaegbu.197108@unn.edu.ng>
2023-06-12 19:29:15 +0100

    update

--- CV/settings.py
+++ CV/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'CV/assets'),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'CV/assets'), os.path.join(BASE_DIR, 'resumee/assets')]
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
